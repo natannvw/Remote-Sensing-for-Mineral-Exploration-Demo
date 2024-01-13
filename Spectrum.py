713b3ab32f43bb99eefb38ee8b3fba7698df4368
NathanTierraSpec <natannvw@gmail.com>
2024-01-13 21:41:53 +0200

    Update Spectrum.py

--- Spectrum.py
+++ Spectrum.py
@@ -1,7 +1,23 @@
+import numpy as np
+import pandas as pd
+
+
 class Spectrum:
-    def __init__(self, wavelength, reflectance):
-        self._wavelength = wavelength
-        self._reflectance = reflectance
+    def __init__(self, mineral_name: str = None):
+        self.mineral_name = mineral_name
+
+        if self.mineral_name:
+            if self.mineral_name == "kaolinite":  # TODO add more minerals
+                path = r"Spectral Library\Kaolinite.txt"
+                df = pd.read_csv(
+                    path,
+                    skiprows=2,
+                    header=None,
+                    names=["wavelength", self.mineral_name],
+                )
+
+                self._wavelength = df["wavelength"].to_numpy()
+                self._reflectance = df[self.mineral_name].to_numpy()
 
     @property
     def wavelength(self):
@@ -18,3 +34,15 @@ class Spectrum:
     @reflectance.setter
     def reflectance(self, value):
         self._reflectance = value
+
+    def resample(self, desired_wavelengths):
+        resampled = np.interp(desired_wavelengths, self.wavelength, self.reflectance)
+        self.wavelength = desired_wavelengths
+        self.reflectance = resampled
+
+        return self
+
+    def preprocess(self, raster):
+        self.resample(desired_wavelengths=raster.wavelength)
+        # self = removeBands(self, "Wavelength", [1, 2.5]) # dont need because it was resampled to raster.wavelength already
+        return self
