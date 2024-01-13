60d31da269b6b38af770686b242f0c55d5bac5fc
NathanTierraSpec <natannvw@gmail.com>
2024-01-13 21:18:49 +0200

    Create Raster.py

---
+++ Raster.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+class Raster:
+    def __init__(self, wavelength, datacube, metadata, profile, name=None, path=None):
+        self.wavelength = wavelength
+        self._datacube = datacube
+        self.metadata = metadata
+        self.profile = profile
+        self.name = name
+        self.path = path
+
+    @property
+    def datacube(self):
+        return self._datacube
+
+    @datacube.setter
+    def datacube(self, value):
+        self._datacube = value
+
+    @staticmethod
+    def rescale(self: "Raster"):
+        gains, offsets = self.get_gains_and_offsets()
+
+        self.datacube = self.datacube * gains[:, None, None] + offsets[:, None, None]
+
+        return self
+
+    @staticmethod
+    def get_gains_and_offsets(self):
+        band_characterisation = self.metadata_dict["level_X"]["specific"][
+            "bandCharacterisation"
+        ]
+
+        band_ids = band_characterisation["bandID"]
+
+        # Extracting gain values
+        gains = [band["GainOfBand"] for band in band_ids]
+
+        # Extracting offset values
+        offsets = [band["OffsetOfBand"] for band in band_ids]
+
+        return np.array(gains, dtype=np.float32), np.array(offsets, dtype=np.float32)
